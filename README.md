a27f9ac52ad605dfd3ec604ad42cbf0ce7ec6212
Jeremy Klein <jklein24@gmail.com>
2024-08-06 01:05:26 -0700

    Initial commit of the uma auth cli tool.

---
+++ README.md
@@ -0,0 +1,71 @@
+# UMA Auth CLI
+
+A simple CLI tool for working with UMA auth. Can help with generating new nostr keys and publishing client app info events.
+
+## Installation
+
+To run the CLI from source, you can clone this repository and run:
+
+```bash
+pipenv install --dev
+pipenv run python -m build
+pip install dist/*.whl
+```
+
+You can then run the CLI from anywhere:
+
+```bash
+uma-auth-cli --help
+```
+
+To install the CLI as a package from pypi instead, you can run:
+
+```bash
+# Note: This is not yet published to pypi.
+pip install uma-auth-cli
+```
+
+## Usage
+
+### Key Generation
+
+To generate a new nostr keypair, run:
+
+```bash
+uma-auth-cli generate-key
+```
+
+It will create and print a new keypair in hex and bech58 format:
+
+```bash
+nsec1e792rulwmsjanw783x39r8vcm23c2hwcandwahaw6wh39rfydshqxhfm7x
+npub13msd7fakpaqerq036kk0c6pf9effz5nn5yk6nqj4gtwtzr5l6fxq64z8x5
+
+sec hex: cf8aa1f3eedc25d9bbc789a2519d98daa3855dd8ecdaeedfaed3af128d246c2e
+pub hex: 8ee0df27b60f419181f1d5acfc68292e52915273a12da9825542dcb10e9fd24c
+```
+
+### Publishing client app info
+
+You can publish a client app info (both kind 0 and 13195) using the CLI. The CLI can publish these events in interactive prompt mode:
+
+```bash
+$ uma-auth-cli publish \
+--nsec nsec1mqxnulkqkcv0gc0dfrxz5kz7d3h665ve2dhjkrj8jmmxwm4st2zsjv2n5l \
+--relay wss://nos.lol,wss://relay.primal.net \
+--redirect-uri https://foo.test \
+--image https://foo.com/image.png \
+--nip05 _@foo.com \
+--name "Test CLI" \
+--description "A test client app"
+```
+
+### Looking up client app info
+
+You can look up client app info using the CLI:
+
+```bash
+$ uma-auth-cli lookup \
+--npub npub13msd7fakpaqerq036kk0c6pf9effz5nn5yk6nqj4gtwtzr5l6fxq64z8x5 \
+--relay wss://nos.lol
+```
---
+++ cli/cli.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# Copyright ©, 2024, Lightspark Group, Inc. - All Rights Reserved
+# pyre-strict
+
+import asyncio
+import json
+from datetime import timedelta
+
+import typer
+from nostr_sdk import (
+    Client,
+    EventBuilder,
+    Filter,
+    Keys,
+    Kind,
+    Metadata,
+    MetadataRecord,
+    NostrSigner,
+    PublicKey,
+)
+from typing_extensions import Annotated
+
+app = typer.Typer(
+    name="UMA Auth CLI",
+    short_help="A CLI helper for managing UMA Auth Client Apps.",
+)
+
+
+@app.command(help="Generate a new nostr keypair.")
+def generate_key() -> None:
+    print("Generating new keypair...")
+    keys = Keys.generate()
+    nsec = keys.secret_key()
+    npub = keys.public_key()
+    print(nsec.to_bech32())
+    print(npub.to_bech32())
+    print(f"nsec hex: {nsec.to_hex()}")
+    print(f"npub hex: {npub.to_hex()}")
+
+
+@app.command(help="Publish client app identity events to the given relays.")
+def publish(
+    nsec: Annotated[
+        str,
+        typer.Option(
+            "--nsec",
+            "-s",
+            help="Secret key to sign the event with. Can be hex or bech32.",
+            prompt=True,
+        ),
+    ],
+    relays: Annotated[
+        list[str],
+        typer.Option(
+            "--relay",
+            "-r",
+            help="Relay to publish to.",
+        ),
+    ],
+    name: Annotated[
+        str,
+        typer.Option(
+            "--name",
+            "-n",
+            help="Name of the client app.",
+            prompt=True,
+        ),
+    ],
+    image: Annotated[
+        str,
+        typer.Option(
+            "--image",
+            "-i",
+            help="URL of the client app image.",
+            prompt=True,
+        ),
+    ],
+    nip05: Annotated[
+        str,
+        typer.Option(
+            "--nip05",
+            "-5",
+            help="NIP-05 address of the client app.",
+            prompt=True,
+        ),
+    ],
+    redirect_uris: Annotated[
+        list[str],
+        typer.Option(
+            "--redirect-uri",
+            "-u",
+            help="Redirect URI of the client app.",
+        ),
+    ],
+    description: Annotated[
+        str,
+        typer.Option(
+            "--description",
+            "-d",
+            help="Description of the client app.",
+        ),
+    ] = "",
+    include_kind_0: Annotated[
+        bool,
+        typer.Option(
+            "--include-0",
+            "-p",
+            help="Publish kind 0 event.",
+        ),
+    ] = True,
+    include_kind_13195: Annotated[
+        bool,
+        typer.Option(
+            "--13195",
+            "-c",
+            help="Publish kind 13195 event.",
+        ),
+    ] = True,
+) -> None:
+    keys = Keys.parse(nsec)
+    print("Publishing client app identity events...")
+    print(f"Name: {name}")
+    print(f"Image: {image}")
+    print(f"NIP-05: {nip05}")
+    print(f"Description: {description}")
+    print(f"Redirect URIs: {redirect_uris}")
+    print(f"Include kind 0: {include_kind_0}")
+    print(f"Include kind 13195: {include_kind_13195}")
+    typer.confirm("This will override any existing events. Are you sure?", abort=True)
+    asyncio.run(
+        publish_client_app_info(
+            keys=keys,
+            relays=relays,
+            name=name,
+            image=image,
+            nip05=nip05,
+            redirect_uris=redirect_uris,
+            description=description,
+            include_kind_0=include_kind_0,
+            include_kind_13195=include_kind_13195,
+        )
+    )
+
+
+async def publish_client_app_info(
+    keys: Keys,
+    relays: list[str],
+    name: str,
+    image: str,
+    nip05: str,
+    redirect_uris: list[str],
+    description: str,
+    include_kind_0: bool,
+    include_kind_13195: bool,
+):
+    signer = NostrSigner.keys(keys)
+    client = Client(signer)
+    await client.add_relays(relays)
+    await client.connect()
+
+    if include_kind_0:
+        print("Publishing kind 0 event...")
+        await client.set_metadata(
+            Metadata.from_record(
+                r=MetadataRecord(
+                    name=name,
+                    display_name=name,
+                    picture=image,
+                    nip05=nip05,
+                    about=description,
+                )
+            )
+        )
+    if include_kind_13195:
+        builder = EventBuilder(
+            kind=Kind(13195),
+            content=json.dumps(
+                {
+                    "name": name,
+                    "image": image,
+                    "nip05": nip05,
+                    "description": description,
+                    "allowed_redirect_uris": redirect_uris,
+                }
+            ),
+            tags=[],
+        )
+        print("Publishing kind 13195 event...")
+        await client.send_event_builder(builder)
+
+    await client.disconnect()
+    print("Events published")
+
+
+@app.command(help="Lookup client app info by npub and relay.")
+def lookup(
+    npub: Annotated[
+        str,
+        typer.Option(
+            "--npub",
+            "-p",
+            help="Public key to lookup the client app info.",
+            prompt=True,
+        ),
+    ],
+    relay: Annotated[
+        str,
+        typer.Option(
+            "--relay",
+            "-r",
+            help="Relay to lookup the client app info.",
+            prompt=True,
+        ),
+    ],
+) -> None:
+    print("Looking up client app info...")
+    public_key = PublicKey.parse(npub)
+    asyncio.run(lookup_client_app_info(public_key, relay))
+
+
+async def lookup_client_app_info(public_key: PublicKey, relay: str):
+    client = Client()
+    await client.add_relays([relay])
+    await client.connect()
+    filter = Filter().kinds([Kind(0), Kind(13195)]).author(public_key)
+    events = await client.get_events_of([filter], timedelta(seconds=10))
+    await client.disconnect()
+    if not events:
+        print("No events found")
+        return
+
+    for event in events:
+        print(json.dumps(json.loads(event.content()), indent=2))
+
+
+if __name__ == "__main__":
+    app()
